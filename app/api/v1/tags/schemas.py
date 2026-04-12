419f2551c48012f53c16eea43fa89dba26cdfc7b
jseb99 <sebastian.mt99@gmail.com>
2026-04-12 14:21:45 -0500

    wip: schemas tags

---
+++ app/api/v1/tags/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class TagPublic(BaseModel):
+    id: int
+    name: str = Field(
+        min_length=2,
+        max_length=30,
+        description="Nombre de la etiqueta"
+    )
+    # Acepte objetos del ORM
+    model_config = ConfigDict(from_attributes=True)
+
+
+class TagCreate(BaseModel):
+    name: str = Field(
+        min_length=2,
+        max_length=30,
+        description="Nombre de la etiqueta"
+    )
+
+
+class TagUpdate(BaseModel):
+    name: str = Field(
+        min_length=2,
+        max_length=30,
+        description="Nombre de la etiqueta"
+    )
+
+
+class TagWithCount(TagPublic):
+    uses: int  # En cuantos posts aparece
